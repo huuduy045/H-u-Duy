90f9aa64064e53986a0bf810ab0e89dc960ae473
huuduy045 <117958709+huuduy045@users.noreply.github.com>
2024-01-04 20:09:13 +0700

    Delete 8.1.py

--- 8.1.py
+++
@@ -1,9 +0,0 @@
-a=input(" nhap a = ")
-b=input(" nhap b = ")
-c=input(" nhap c = ")
-d=input(" nhap d = ")
-dayso=[a,b,c,d]
-print("Số lớn nhất là:")
-print(max(dayso))
-print("Số nhỏ nhất là:")
-print(min(dayso))
